bbae5ef53a15e0f9d6b1bbc86f42a4652eaf5298
Vika Nika <veronika.0983@inbox.ru>
2024-11-24 11:39:08 +0300

    first commit3

--- src/components/Header/MobileMenu/index.tsx
+++ src/components/Header/MobileMenu/index.tsx
@@ -16,16 +16,16 @@ export const MobileMenu = () => {
       {isOpen && (
         <div className={styles.wrapper}>
           <nav>
-            <Link to="/hw18/BeansProject/beans" onClick={handleLinkClick}>
+            <Link to="/hw18/beans" onClick={handleLinkClick}>
               Beans
             </Link>
-            <Link to="/hw18/BeansProject/facts" onClick={handleLinkClick}>
+            <Link to="/hw18/facts" onClick={handleLinkClick}>
               Facts
             </Link>
-            <Link to="/hw18/BeansProject/recipes" onClick={handleLinkClick}>
+            <Link to="/hw18/recipes" onClick={handleLinkClick}>
               Recipes
             </Link>
-            <Link to="/hw18/BeansProject/combinations" onClick={handleLinkClick}>
+            <Link to="/hw18/combinations" onClick={handleLinkClick}>
               Combinations
             </Link>
             <Link to="/hw18/BeansProject/history" onClick={handleLinkClick}>
